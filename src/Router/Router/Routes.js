179af8d28d4ed935fe6b7792e18350c970155ac9
hasibulhasan44 <hasanhasib7367@gmail.com>
2022-12-02 20:51:48 +0600

    blogs page done

--- src/Router/Router/Routes.js
+++ src/Router/Router/Routes.js
@@ -1,26 +1,27 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import Main from "../../Layout/Main/Main";
-import Blogs from "../../Pages/Blogs/Blogs";
-import Home from "../../Pages/Home/Home";
-import Login from "../../Pages/Login/Login";
-import SignUp from "../../Pages/SignUp/SignUp";
-import ListAnItem from "../../Pages/DashBoard/ListAnItem/ListAnItem";
-import DashboardLayout from "../../Layout/Main/DashboardLayout/DashboardLayout";
-import MyListings from "../../Pages/DashBoard/MyListings/MyListings";
-import EditItem from "../../Pages/DashBoard/EditItem/EditItem";
+import Main from "../Layout/Main/Main";
+import Blogs from "../Pages/Blogs/Blogs";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
+import ListAnItem from "../Pages/DashBoard/ListAnItem/ListAnItem";
+import DashboardLayout from "../Layout/Main/DashboardLayout/DashboardLayout";
+import MyListings from "../Pages/DashBoard/MyListings/MyListings";
+import EditItem from "../Pages/DashBoard/EditItem/EditItem";
 import { async } from "@firebase/util";
-import Brands from "../../Pages/Phones/Brands/Brands";
-import PhoneDetails from "../../Pages/Phones/PhoneDetails/PhoneDetails";
-import AllPhones from "../../Pages/Phones/AllPhones/AllPhones";
-import Phones from "../../Pages/Phones/Phones/Phones";
-import MyCustomers from "../../Pages/DashBoard/MyCustomers/MyCustomers";
-import AllUser from "../../Pages/DashBoard/AdminDashboard/AllUser/AllUser";
-import AllBuyer from "../../Pages/DashBoard/AdminDashboard/AllBuyer/AllBuyer";
-import AllSeller from "../../Pages/DashBoard/AdminDashboard/AllSeller/AllSeller";
-import ReportedPhones from "../../Pages/DashBoard/AdminDashboard/ReportedPhones/ReportedPhones";
-import MyOrders from "../../Pages/DashBoard/BuyerDashboard/MyOrders/MyOrders";
-import MyWishlist from "../../Pages/DashBoard/BuyerDashboard/MyWishlist/MyWishlist";
+import Brands from "../Pages/Phones/Brands/Brands";
+import PhoneDetails from "../Pages/Phones/PhoneDetails/PhoneDetails";
+import AllPhones from "../Pages/Phones/AllPhones/AllPhones";
+import Phones from "../Pages/Phones/Phones/Phones";
+import MyCustomers from "../Pages/DashBoard/MyCustomers/MyCustomers";
+import AllUser from "../Pages/DashBoard/AdminDashboard/AllUser/AllUser";
+import AllBuyer from "../Pages/DashBoard/AdminDashboard/AllBuyer/AllBuyer";
+import AllSeller from "../Pages/DashBoard/AdminDashboard/AllSeller/AllSeller";
+import ReportedPhones from "../Pages/DashBoard/AdminDashboard/ReportedPhones/ReportedPhones";
+import MyOrders from "../Pages/DashBoard/BuyerDashboard/MyOrders/MyOrders";
+import MyWishlist from "../Pages/DashBoard/BuyerDashboard/MyWishlist/MyWishlist";
+import SellerRoute from "../SellerRoute/SellerRoute";
 
 export const routes = createBrowserRouter([
   {
@@ -70,11 +71,11 @@ export const routes = createBrowserRouter([
     children:[
       {
         path:'/dashboard/listanitem',
-        element:<ListAnItem></ListAnItem>
+        element:<SellerRoute><ListAnItem></ListAnItem></SellerRoute>
       },
       {
         path:'/dashboard/mylistings',
-        element:<MyListings></MyListings>
+        element:<SellerRoute><MyListings></MyListings></SellerRoute>
       },
       {
         path: '/dashboard/edititem/:id',
